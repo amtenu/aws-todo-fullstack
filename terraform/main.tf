9a7726073e1d995730689d8a33d489d005fd0607
amtenu <amannov21@gmail.com>
2026-03-13 15:55:38 -0600

    feat: add RDS MySQL database
    Database configuration:
    - Engine: MySQL 8.0
    - Instance: db.t3.micro
    - Storage: 20 GB gp3 (auto-scales to 100 GB)
    - Backups: 1 day retention
    - Network: Private subnets only
    - Security: RDS security group (port 3306 from ECS only)
    - Multi-AZ: Disabled
    - Endpoint: todoapp-db.cx6dwzac9bss.ca-west-1.rds.amazonaws.com:3306
    - Database name: todoapp

--- terraform/main.tf
+++ terraform/main.tf
@@ -304,3 +304,73 @@ output "rds_security_group_id" {
 }
 
 
+
+# RDS DATABASE 
+
+# DB Subnet Group 
+resource "aws_db_subnet_group" "main" {
+  name       = "todoapp-db-subnet-group"
+  subnet_ids = [aws_subnet.private_1.id, aws_subnet.private_2.id]
+
+  tags = {
+    Name = "todoapp-db-subnet-group"
+  }
+}
+
+# RDS MySQL Instance
+resource "aws_db_instance" "main" {
+  identifier     = "todoapp-db"
+  engine         = "mysql"
+  engine_version = "8.0"
+
+
+  instance_class = "db.t3.micro"
+
+
+  allocated_storage     = 20 # GB
+  max_allocated_storage = 100
+  storage_type          = "gp3"
+  storage_encrypted     = true
+
+  # Database
+  db_name  = "todoapp"
+  username = "todouser"
+  password = "TodoApp2026SecurePassword!" # will use secret manager
+  port     = 3306
+
+  # Network
+  db_subnet_group_name   = aws_db_subnet_group.main.name
+  vpc_security_group_ids = [aws_security_group.rds.id]
+  publicly_accessible    = false # Keep private!
+
+  # Backups
+  backup_retention_period = 1
+  backup_window           = "03:00-04:00"
+  maintenance_window      = "mon:04:00-mon:05:00"
+
+  # High Availability
+  multi_az = false # Will be true for production
+
+  # Deletion protection
+  deletion_protection       = false # will be set to true for production
+  skip_final_snapshot       = true  # ill be set false for production
+  final_snapshot_identifier = "todoapp-db-final-snapshot"
+
+  # Performance
+  enabled_cloudwatch_logs_exports = ["error", "general", "slowquery"]
+
+  tags = {
+    Name = "todoapp-db"
+  }
+}
+
+# RDS Outputs
+output "rds_endpoint" {
+  description = "RDS endpoint"
+  value       = aws_db_instance.main.endpoint
+}
+
+output "rds_database_name" {
+  description = "Database name"
+  value       = aws_db_instance.main.db_name
+}
